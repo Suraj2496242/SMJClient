7dbf1dfc06d3c02890e3a8c0a69345643f9a292e
Suraj Vernekar <suraj.vernekar@continental-corporation.com>
2016-01-13 15:53:55 +0530

    Sample consumer code
    Change-Id: I88d255571f7189c88734bd8d8fddca5da6495794

--- src/Consumer_Test.java
+++ src/Consumer_Test.java
@@ -1,4 +1,38 @@
 
-public class Consumer_Test {
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
 
+public class Consumer_Test {
+ 
+	public static void main(String args[])
+	{
+		try {
+
+			Client client = Client.create();
+
+			WebResource webResource = client
+			   .resource("http://localhost:8180/SMJ/operations/ftocservice");
+
+			ClientResponse response = webResource.accept("application/json")
+	                   .get(ClientResponse.class);
+
+			if (response.getStatus() != 200) {
+			   throw new RuntimeException("Failed : HTTP error code : "
+				+ response.getStatus());
+			}
+
+			String output = response.getEntity(String.class);
+
+			System.out.println("Output from Server .... \n");
+			System.out.println(output);
+
+		  } catch (Exception e) {
+
+			e.printStackTrace();
+
+		  }
+
+		
+	}
 }
